d47d5c5eb11739fdb2feb272b1e97645c266cd59
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-02-20 15:57:42 -0600

    combined commands

--- node.up.sh
+++ node.up.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
-docker-compose $(ls docker-compose*yml | awk '{printf "-f %s ", $0}') up --build -d --remove-orphans
+if [ -e 'docker-compose.plugins.yml' ]; then
+    compose_file_args='-f docker-compose.system.yml -f docker-compose.plugins.yml'
+else
+    compose_file_args='-f docker-compose.system.yml'
+fi
 
-while ! python3 shovelctl.py enable; do
-    sleep 3
-done
+command_up() {
+    docker-compose $compose_file_args up --build -d --remove-orphans
+
+    while ! python3 shovelctl.py enable; do
+        sleep 3
+    done
+}
+
+command_down() {
+    docker-compose $compose_file_args down --remove-orphans
+}
+
+command_logs() {
+    docker-compose $compose_file_args logs -f
+}
+
+command="$1"
+shift
+
+case "$command" in
+    up) command_up $@ ;;
+    down) command_down $@ ;;
+    logs) command_logs $@ ;;
+esac
