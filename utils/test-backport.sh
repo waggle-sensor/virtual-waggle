a0f28acf7f2a3e834fb26a4e778a45fb0f45b77f
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-05-22 10:41:28 -0500

    added script to spin up stack on aot node

---
+++ utils/test-backport.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# TODO add restart policy
+
+hostname=node50288
+
+# rsync -av /Users/sean/waggle-node/services/rabbitmq-node/ $hostname:/wagglerw/rabbitmq-node/
+# ssh $hostname docker build -t waggle/rabbitmq:nc /wagglerw/rabbitmq-node/
+
+ssh $hostname bash -c '
+docker rm -f rabbitmq
+
+echo "starting rabbitmq permissions"
+docker run -d --restart always --network waggle \
+    -v /etc/waggle/cacert.pem:/run/waggle/cacert.pem:ro \
+    -v /etc/waggle/cert.pem:/run/waggle/cert.pem:ro \
+    -v /etc/waggle/key.pem:/run/waggle/key.pem:ro \
+    --name rabbitmq \
+    waggle/rabbitmq:nc
+
+echo "fixing credential permissions"
+docker exec -i --user root rabbitmq bash -c "mkdir -p /etc/waggle; cp /run/waggle/* /etc/waggle/; chown -R rabbitmq:rabbitmq /etc/waggle"
+
+echo "setting up shovels"
+docker run -i --network waggle --rm -e WAGGLE_NODE_ID=0000001e06118459 -e WAGGLE_SUB_ID=0000000000000000 -e WAGGLE_BEEHIVE_HOST=140.221.47.67 waggle/shovelctl python3 /app/shovelctl.py enable
+
+echo "starting metsense plugin"
+docker rm -f plugin-metsense
+docker run -d --restart always --network waggle --device /dev/waggle_coresense --name plugin-metsense waggle/plugin-metsense:4.1.0
+'
