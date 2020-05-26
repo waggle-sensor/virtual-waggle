b5278ce233153232cab376b11713b33f9292ce4f
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-05-26 16:59:49 -0500

    rename services

--- services/build-all.sh
+++ services/build-all.sh
@@ -4,7 +4,7 @@ build_service() {
     docker buildx build -t "$1" --platform linux/amd64,linux/arm64,linux/arm/v7 --push "$2"
 }
 
-build_service waggle/rabbitmq:nc rabbitmq-node
-build_service waggle/message-staging-node:nc message-staging-node
+build_service waggle/rabbitmq:nc rabbitmq-nc
+build_service waggle/message-staging-node:nc message-staging-node-nc
 build_service waggle/shovelctl shovelctl
 build_service waggle/registration registration
