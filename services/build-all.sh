dc144e7ae3a7f068f5900037479cf92fc8365916
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-05-26 17:04:30 -0500

    adding ep rabbitmq config

--- services/build-all.sh
+++ services/build-all.sh
@@ -5,6 +5,7 @@ build_service() {
 }
 
 build_service waggle/rabbitmq:nc rabbitmq-nc
+build_service waggle/rabbitmq:ep rabbitmq-ep
 build_service waggle/message-staging-node:nc message-staging-node-nc
 build_service waggle/shovelctl shovelctl
 build_service waggle/registration registration
