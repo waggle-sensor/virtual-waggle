5228fe59006db52a92db70f5f051b06d9f5e0bf5
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-07-06 17:05:54 -0500

    build plugin accepts multiple pathts

---
+++ utils/build-plugin.py
@@ -0,0 +1,28 @@
+import argparse
+import json
+import subprocess
+from pathlib import Path
+
+parser = argparse.ArgumentParser()
+parser.add_argument('plugins', nargs='+')
+args = parser.parse_args()
+
+images = []
+
+for plugin in args.plugins:
+    config = json.loads(Path(plugin, 'sage.json').read_text())
+
+    image_name = 'plugin-{name}:{version}'.format(**config)
+
+    subprocess.check_output([
+        'docker',
+        'build',
+        '--label', 'waggle.plugin.id={id}'.format(**config),
+        '--label', 'waggle.plugin.version={version}'.format(**config),
+        '-t', image_name,
+        plugin,
+    ])
+
+    images.append(image_name)
+
+print(*images)
