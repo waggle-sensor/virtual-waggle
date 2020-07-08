2dc750fc85a4f75af12550b28d4de2a7ea9079c3
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-07-08 09:09:28 -0500

    match docker run command - will add support for args later. check if arg is a plugin directory.

--- utils/build-plugin.py
+++ utils/build-plugin.py
@@ -2,28 +2,33 @@ import argparse
 import json
 import subprocess
 from pathlib import Path
+import sys
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument('plugins', nargs='+')
+parser.add_argument('plugin_dir')
 args = parser.parse_args()
 
-images = []
+if not Path(args.plugin_dir).is_dir():
+    print('error: argument must point to base directory of a plugin')
+    sys.exit(1)
 
-for plugin in args.plugins:
-    config = json.loads(Path(plugin, 'sage.json').read_text())
+try:
+    config = json.loads(Path(args.plugin_dir, 'sage.json').read_text())
+except FileNotFoundError:
+    print('error: plugin is missing sage.json metadata file')
+    sys.exit(1)
 
-    image_name = 'plugin-{name}:{version}'.format(**config)
+image_name = 'plugin-{name}:{version}'.format(**config)
 
-    subprocess.check_output([
-        'docker',
-        'build',
-        '--label', 'waggle.plugin.id={id}'.format(**config),
-        '--label', 'waggle.plugin.version={version}'.format(**config),
-        '--label', 'waggle.plugin.name={name}'.format(**config),
-        '-t', image_name,
-        plugin,
-    ])
+subprocess.check_output([
+    'docker',
+    'build',
+    '--label', 'waggle.plugin.id={id}'.format(**config),
+    '--label', 'waggle.plugin.version={version}'.format(**config),
+    '--label', 'waggle.plugin.name={name}'.format(**config),
+    '-t', image_name,
+    args.plugin_dir,
+])
 
-    images.append(image_name)
-
-print(*images)
+print(image_name)
