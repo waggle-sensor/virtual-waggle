e594227e979eb39d302362494477e45edb91f2d5
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-08-17 10:54:57 -0500

    removed internal tests

--- waggle_node_test.py
+++ waggle_node_test.py
@@ -68,40 +68,9 @@ class TestUtils(unittest.TestCase):
             'id': 123,
             'name': 'test',
             'version': '1.2.3',
-            'build_args': {
-                'K1': 'V1',
-                'K2': 'V2',
-                'K3': 'V3',
-            }
         })
 
         self.assertEqual(name, 'plugin-test:1.2.3')
-
-    def test_build_args_from_list(self):
-        r = waggle_node.get_build_args_from_list([
-            'ARG1=the',
-            'ARG2=colors',
-            'ARG3=duke',
-        ])
-
-        self.assertEqual(r, [
-            '--build-arg', 'ARG1=the',
-            '--build-arg', 'ARG2=colors',
-            '--build-arg', 'ARG3=duke',
-        ])
-
-    def test_build_args_from_config(self):
-        r = waggle_node.get_build_args_from_dict({
-            'ARG1': 'the',
-            'ARG2': 'colors',
-            'ARG3': 'duke',
-        })
-
-        self.assertEqual(r, [
-            '--build-arg', 'ARG1=the',
-            '--build-arg', 'ARG2=colors',
-            '--build-arg', 'ARG3=duke',
-        ])
 
 
 if __name__ == '__main__':
